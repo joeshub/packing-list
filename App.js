20e92d2287dd2517f8e2eccaf4f923933aab759a
fvonhoven <frank@infinite.red>
2018-06-12 00:29:13 -0500

    Able to add & clear list items

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react"
-import { StyleSheet, Text, View, TextInput } from "react-native"
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native"
+
+/*
+  STEP TWO
+  • Move the input to its own function and a wrapper & some buttons
+  • Introduce "items" array into local state
+  • Able to push inputted items into the items array
+  • Able to clear items from the items array
+*/
 
 export default class App extends Component {
   state = {
-    inputValue: null
+    inputValue: null,
+    items: []
   }
 
   handleInput = value => {
     this.setState({ inputValue: value })
   }
 
-  render() {
+  addNewItem() {
+    const { inputValue, items } = this.state
+    const newItems = items.concat(inputValue)
+    this.setState({ items: newItems })
+  }
+
+  clearItems() {
+    this.setState({ items: [] })
+  }
+
+  renderInputRow = () => {
     const { inputValue } = this.state
     return (
+      <View style={styles.inputRow}>
+        <TextInput style={styles.input} value={inputValue} onChangeText={this.handleInput} />{" "}
+        {/* Introduce the new function syntax */}
+        <TouchableOpacity style={styles.addButton} onPress={() => this.addNewItem()}>
+          <Text style={styles.buttonText}>ADD</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.clearButton} onPress={() => this.clearItems()}>
+          <Text style={styles.buttonText}>Clear</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  render() {
+    const { inputValue, items } = this.state
+    return (
       <View style={styles.container}>
-        <TextInput style={styles.input} value={inputValue} onChangeText={this.handleInput} />
-        <Text style={styles.theValue}>{inputValue}</Text>
+        {this.renderInputRow()}
+        {items.map((item, i) => (
+          <Text key={i} style={styles.theValue}>
+            {item}
+          </Text>
+        ))}
       </View>
     )
   }
@@ -33,10 +72,28 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: "lightgray",
     borderWidth: 1,
-    padding: 5
+    padding: 5,
+    fontSize: 16
   },
   theValue: {
     margin: 10,
     fontSize: 18
+  },
+  inputRow: {
+    flexDirection: "row"
+  },
+  addButton: {
+    marginLeft: 10,
+    justifyContent: "center",
+    backgroundColor: "green"
+  },
+  clearButton: {
+    marginLeft: 10,
+    justifyContent: "center",
+    backgroundColor: "gray"
+  },
+  buttonText: {
+    margin: 5,
+    color: "white"
   }
 })
