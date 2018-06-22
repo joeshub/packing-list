408fcddf4beea659a5e48ae8108231029f4bff16
fvonhoven <frank@infinite.red>
2018-06-22 17:27:29 -0500

    Rm unused styles

--- src/components/list-input.js
+++ src/components/list-input.js
@@ -3,7 +3,7 @@ import { View, TextInput, StyleSheet } from "react-native"
 import { Button } from "./button"
 
 export class ListInput extends React.Component {
-  render () {
+  render() {
     const { value, onChangeText, onAddItem, onClearItems } = this.props
     return (
       <View style={styles.inputRow}>
@@ -16,17 +16,6 @@ export class ListInput extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5FCFF"
-  },
-  topContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "black"
-  },
   input: {
     width: "50%",
     height: 40,
@@ -35,57 +24,7 @@ const styles = StyleSheet.create({
     padding: 5,
     fontSize: 16
   },
-  theValue: {
-    margin: 10,
-    fontSize: 18
-  },
   inputRow: {
     flexDirection: "row"
-  },
-  addButton: {
-    borderRadius: 5,
-    marginLeft: 10,
-    shadowRadius: 5,
-    shadowOffset: {
-      width: 5,
-      height: 5
-    },
-    shadowOpacity: 0.9,
-    shadowColor: "black",
-    justifyContent: "center",
-    backgroundColor: "green"
-  },
-  clearButton: {
-    borderRadius: 5,
-    marginLeft: 10,
-    shadowRadius: 5,
-    shadowOffset: {
-      width: 5,
-      height: 5
-    },
-    shadowOpacity: 0.9,
-    shadowColor: "black",
-    justifyContent: "center",
-    backgroundColor: "gray"
-  },
-  buttonText: {
-    margin: 5,
-    color: "white"
-  },
-  list: {
-    borderWidth: 1,
-    borderColor: "lightgray"
-  },
-  itemWrapper: {
-    borderBottomWidth: 1,
-    borderColor: "lightgray",
-    height: 40,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  item: {
-    margin: 5,
-    fontSize: 12
   }
 })
